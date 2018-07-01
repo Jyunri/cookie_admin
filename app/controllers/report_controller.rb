4eb12102af3029aae71d86050f943b27033c5d53
Jyunri <nekojimy@gmail.com>
2018-06-30 21:10:09 -0300

    render multiprint params in view

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,23 +3,23 @@ class ReportController < ApplicationController
 
 	def show
 		if(params[:multiprint])
-			@client_name = JSON.parse(params["client_names"])
-			@client_rg = JSON.parse(params["client_rgs"])
-			@client_cpf = JSON.parse(params["client_cpfs"])
-			@client_phone = JSON.parse(params["client_phones"])
-			@client_address = JSON.parse(params["client_addresses"])
-			@client_neighborhood = JSON.parse(params["client_neighborhoods"])
-			@client_city = JSON.parse(params["client_cities"])
+			@client_names = JSON.parse(params["client_names"])
+			@client_rgs = JSON.parse(params["client_rgs"])
+			@client_cpfs = JSON.parse(params["client_cpfs"])
+			@client_phones = JSON.parse(params["client_phones"])
+			@client_addresses = JSON.parse(params["client_addresses"])
+			@client_neighborhoods = JSON.parse(params["client_neighborhoods"])
+			@client_cities = JSON.parse(params["client_cities"])
 			@orders = JSON.parse(params["orders"])
 		else
-			@client_name = params["client_names"]
-			@client_rg = params["client_rgs"]
-			@client_cpf = params["client_cpfs"]
-			@client_phone = params["client_phones"]
-			@client_address = params["client_addresses"]
-			@client_neighborhood = params["client_neighborhoods"]
-			@client_city = params["client_cities"]
-			@orders = params["orders"]
+			@client_names = { "0" => params["client_names"] }
+			@client_rgs = { "0" => params["client_rgs"] }
+			@client_cpfs = { "0" => params["client_cpfs"] }
+			@client_phones = { "0" => params["client_phones"] }
+			@client_addresses = { "0" => params["client_addresses"] }
+			@client_neighborhoods = { "0" => params["client_neighborhoods"] }
+			@client_cities = { "0" => params["client_cities"] }
+			@orders = { "0" => params["orders"] }
 		end
 	end
 end
